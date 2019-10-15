79368f312cf59111f06d1a9e006e3977f4c35103
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-15 11:05:44 -0600

    configured

--- api/server.js
+++ api/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require("express-session");
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: "ohfosho", //default would be "sid"
+  secret: "keep it secret, keep it safe", // <-- use environment variable for this!!!
+  cookie: {
+    httpOnly: false, //JS cannot access the cookies
+    maxAge: 1000 * 60 * 60, //expiration time in milliseconds
+    // secure: process.env.NODE_ENV === production ? true : false;
+    secure: false //should be true in production
+  }, 
+  resave: false,
+  saveUninitialized: false, //read about GDPR compliance
+}
+
+server.use(sessions(sessionConfiguration));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
